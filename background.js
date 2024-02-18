14c9ea57f036932018f368a6a5ebdbaab4b1f0b9
vsnain <47077796+vsnain@users.noreply.github.com>
2024-02-17 22:33:50 -0500

    Delete background.js

--- background.js
+++
@@ -1,6 +0,0 @@
-// Load Puppeteer 
-const puppeteer = require('puppeteer');
-
-// Load Stealth plugin
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
